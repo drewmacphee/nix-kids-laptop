9aedcd2ff5c06720d6ea2f37882bd07ea85d4c7a
Drew MacPhee <drewjamesross@outlook.com>
2025-12-15 18:06:50 -0500

    Fix flake to use single generic configuration name

--- flake.nix
+++ flake.nix
@@ -11,33 +11,23 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      # Shared configuration for all hosts  
-      # The hostname will be read from /etc/hostname by configuration.nix
-      mkHost = name: nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.drew = import ./home-drew.nix;
-            home-manager.users.emily = import ./home-emily.nix;
-            home-manager.users.bella = import ./home-bella.nix;
-          }
-        ];
-      };
-    in {
-      # Multiple hostname configs that all use the same modules
-      # Add new hostnames here as needed
-      nixosConfigurations = {
-        bazztop = mkHost "bazztop";
-        emily-laptop = mkHost "emily-laptop";
-        bella-laptop = mkHost "bella-laptop";
-        kids-desktop = mkHost "kids-desktop";
-      };
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Single configuration that works for any hostname
+    # The actual hostname is set in configuration.nix from /etc/hostname
+    nixosConfigurations.kids-laptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.drew = import ./home-drew.nix;
+          home-manager.users.emily = import ./home-emily.nix;
+          home-manager.users.bella = import ./home-bella.nix;
+        }
+      ];
     };
+  };
 }
